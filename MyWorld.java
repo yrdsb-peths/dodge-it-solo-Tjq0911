0deb205e92b64cd47cf88de833423cece3a63a6d
Tjq0911 <tujinqi0911@gmail.com>
2025-11-07 14:45:14 -0500

    add player and player's control

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,13 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private int score = 0;
+    private Player player;
     public MyWorld() {
+         
         super(600, 400, 1);
+        Player player = new Player();
+        addObject(player, 300, 350);
+        
     }
 }
