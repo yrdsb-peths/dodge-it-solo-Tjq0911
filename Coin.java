31a6c3b53d357e76cfc590f0a041ac28945dd59a
Tjq0911 <tujinqi0911@gmail.com>
2025-11-08 16:07:51 -0500

    COin

---
+++ Coin.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Coin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Coin extends Actor
+{
+    /**
+     * Act - do whatever the Coin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX(), getY() + 3);
+        if (isTouching(Player.class))
+        {
+            Player player = (Player) getOneIntersectingObject(Player.class);
+            player.catchCoin();
+            getWorld().removeObject(this);
+        }// Add your action code here.
+    }
+}
