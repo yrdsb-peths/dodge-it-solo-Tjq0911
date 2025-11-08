e9440b14684a3be11d413444f257aa5d031706a6
Tjq0911 <tujinqi0911@gmail.com>
2025-11-08 16:55:51 -0500

    timer

--- MyWorld.java
+++ MyWorld.java
@@ -3,12 +3,14 @@ import greenfoot.*;
 public class MyWorld extends World {
     private int score = 0;
     private Player player;
+    private int time = 1800;
     public MyWorld() {
          
         super(600, 400, 1);
         Player player = new Player();
         addObject(player, 300, 350);
         addObject(new Coin(), Greenfoot.getRandomNumber(600), 0);
+        showText("Time: 30", 520, 20);
         
     }
     public void act()
@@ -16,6 +18,11 @@ public class MyWorld extends World {
         if(Greenfoot.getRandomNumber(100) < 2)
         {
             addObject(new Coin(), Greenfoot.getRandomNumber(600), 0);    
+        }
+        time--;
+        if(time % 60 == 0)
+        {
+            showText("Time: " + (time / 60), 520, 20);    
         }
     }
     public void addScore(int points)
