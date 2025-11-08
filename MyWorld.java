167cdd5492842e495f622b9d7f2e1cd7a57fdf3d
Tjq0911 <tujinqi0911@gmail.com>
2025-11-08 16:06:13 -0500

    coin falling

--- MyWorld.java
+++ MyWorld.java
@@ -8,6 +8,14 @@ public class MyWorld extends World {
         super(600, 400, 1);
         Player player = new Player();
         addObject(player, 300, 350);
+        addObject(new Coin(), Greenfoot.getRandomNumber(600), 0);
         
+    }
+    public void act()
+    {
+        if(Greenfoot.getRandomNumber(100) < 2)
+        {
+            addObject(new Coin(), Greenfoot.getRandomNumber(600), 0);    
+        }
     }
 }
